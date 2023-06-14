e0254d18c6cc7ec457c2b752a5de224425046bcf
Fery-K <fery.kurniiawan172@gmail.com>
2023-06-14 13:58:51 +0700

    commit

--- Dashboard.py
+++ Dashboard.py
@@ -1,7 +1,11 @@
 import streamlit as st
 import pandas as pd
-import altair as alt
+import matplotlib.pyplot as plt
+import seaborn as sns
+from numerize import numerize
+from adjustText import adjust_text
+import geopandas as gpd
 
-st.set_page_config(page_title="Capstone Project", layout="wide")
+st.set_page_config(page_title="Capstone Project Dashboard", layout="wide")
 
 st.title('Pengaruh Pemberian Kredit Sebagai Pemicu Pertumbuhan Produk Regional Domestik Bruto')
