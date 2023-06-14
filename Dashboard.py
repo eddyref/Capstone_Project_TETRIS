e19d105ab785e4626fd2517e3d5c31323acaa106
Fery-K <fery.kurniiawan172@gmail.com>
2023-06-15 03:03:11 +0700

    commit

--- Dashboard.py
+++ Dashboard.py
@@ -64,9 +64,8 @@ kp_pdrb = pd.merge(kp[kp['tahun'] >= 2011].reset_index(),
                    on=['provinsi', 'tahun'])
 
 k_pdrb = pd.merge(k_pivot[k_pivot['tahun'] >= 2011],
-                  pdrb_ver.groupby('tahun')[['tahun', 'PDRB']].sum(),
+                  pdrb_ver.groupby('tahun')['PDRB'].sum(),
                   on='tahun')
-k_pdrb = k_pdrb.reset_index().drop(columns='tahun')
 
 # Title and Prologue
 
